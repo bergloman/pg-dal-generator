281ee837a9a5c65127598414a7c122d9563f65ea
Viktor Jovanoski <viktor@carvic.si>
2018-11-30 13:30:29 +0100

    small work

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,10 @@
-import { DbAccess } from "./pg_acces";
+import { DbAccess } from "./pg_access";
 
-const map: Map<string, string> = new Map<string, string>();
-
-map.set("int4", "numeric");
-map.set("float8", "numeric");
-map.set("timestamp", "Date");
-map.set("varchar", "string");
-map.set("bool", "boolean");
-map.set("json", "any");
-
-async function run(con_str: string, schema: string) {
+async function run(con_str: string, schema: string, map: Map<string, string>): Promise<void> {
     const db = DbAccess.create(con_str);
 
     const res = await db.any(`SELECT * FROM information_schema.tables where table_schema = '${schema}';`);
-const Consts = {};
+    const Consts = {};
     for (const tab of res) {
 
         console.log("class", /*tab.table_schema,*/ tab.table_name, "{");
@@ -27,8 +18,8 @@ const Consts = {};
             if (field.column_name == "id") {
                 continue;
             }
-            console.log(`    ${field.column_name}: ${map.get(field.udt_name) || "ERROR=" + field.udt_name};`);
-            //console.log(field);
+            const output_type = map.get(field.udt_name) || "ERROR=" + field.udt_name;
+            console.log(`    ${field.column_name}: ${output_type};`);
             tabFields[field.column_name] = field.column_name;
         }
         console.log("}")
@@ -37,5 +28,20 @@ const Consts = {};
     console.log(Consts);
 }
 
+function prepareTypeMapping() {
+    const map: Map<string, string> = new Map<string, string>();
+    map.set("int4", "numeric");
+    map.set("float8", "numeric");
+    map.set("timestamp", "Date");
+    map.set("varchar", "string");
+    map.set("bool", "boolean");
+    map.set("json", "any");
+    return map;
+}
 
+///////////////////////////////////////////////
+
+const map: Map<string, string> = prepareTypeMapping();
+
+run("......", ".....", map);
 
